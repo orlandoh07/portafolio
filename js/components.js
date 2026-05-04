cb050fafdfefdc707cc588b17310bd67466c4605
Orlando Herrera <orlandoherrera2191@gmail.com>
2026-05-03 22:36:25 -0400

    skills section design

--- js/components.js
+++ js/components.js
@@ -8,3 +8,22 @@ const navbar = document.querySelector('.my-nav');
             navbar.classList.remove('scrolled');
         }
         });
+
+//animacion de la lista de skills
+const skillHeaders = document.querySelectorAll('.skill-header');
+let skillActivo = null;
+for (const header of skillHeaders) {
+    header.addEventListener('click', () => {
+        const skillItem = header.parentElement;
+        if (skillActivo !== null) {
+            skillActivo.classList.remove('activo');
+        }
+        if (skillActivo === skillItem) {
+            skillActivo = null;
+        }
+        else {
+            skillItem.classList.add('activo');
+            skillActivo = skillItem;
+        }
+    });
+}
